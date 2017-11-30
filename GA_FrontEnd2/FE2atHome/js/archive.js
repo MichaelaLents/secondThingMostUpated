81eb082a6b6def340bfc2bf54abac9af908511ce
Michaela Lents <michaela.lents2@capitalone.com>
2017-11-30 10:27:36 -0500

    Finshes part3

--- GA_FrontEnd2/FE2atHome/js/archive.js
+++ GA_FrontEnd2/FE2atHome/js/archive.js
@@ -1,3 +1,103 @@
+
+(function() {
+  'use strict';
+
+  let offset = 0;
+
+  let url = 'https://credentials-api.generalassemb.ly/explorer/posts?offset=';
+  var jsonURL = 'https://credentials-api.generalassemb.ly/explorer/posts';
+
+  let $loadMoreButton = $('#load-more');
+  let $articleSection = $('#article-list article');
+
+//   $.ajaxSetup({
+//   beforeSend: function() {
+//      // $('#loader').show();
+//      $loadMoreButton.html('Exploring the archive...'+'<i class="fa fa-circle-o-notch fa-spin">');
+//      console.log('Showing the loader');
+//   },
+//   complete: function(){
+//      // $('#loader').hide();
+//      $loadMoreButton.text('End of archive');
+//      console.log('Hiding the loader');
+//   }
+// });
+
+
+  function registerLoadMoreButton(){
+
+    $loadMoreButton.on('click', function(){
+      console.log('clicked the button');
+
+      getMoreArticles();
+    });
+  }
+
+  function getMoreArticles(){
+
+    $.ajax({type: "GET",
+           url: url+offset,
+           beforeSend: function() {
+              $loadMoreButton.html('Exploring the archive...'+'<i class="fa fa-circle-o-notch fa-spin">');
+              console.log('Showing the loader');
+              $loadMoreButton.attr('disabled', true);
+
+              console.log('Url: ', url+offset);
+              debugger;
+           },
+            complete: function(){
+
+             }
+           ,
+           success: function(data){
+             console.log('Have received the data: ', JSON.parse(data));
+             offset+=4;
+             let json = JSON.parse(data);
+
+             if(json.posts.length){
+               $loadMoreButton.text('Load More');
+
+               $loadMoreButton.attr('disabled', false);
+               addNewArticles(json.posts);
+             }else{
+               $loadMoreButton.text('End of archive');
+               console.log('No more posts!');
+             }
+           }
+    });
+  }
+
+  function addNewArticles(posts){
+
+    posts.forEach(function(post){
+      let newPost = createNewArticle(post);
+      $articleSection = $('#article-list article');
+
+      $articleSection.last().after(newPost);
+    });
+  }
+
+  function createNewArticle(post){
+    let $article = $('<article>');
+    let $i = $('<i>').addClass(post.category);
+    let $h2 = $('<h2>').text('From the Archive');
+    let $h1 = $('<h1>').text(post.title);
+    let $h3 = $('<h3>').text(post.date);
+    let $p = $('<p>').text(post.blurb);
+
+
+    $article.append($i).append($h2).append($h1).append($h3).append($p);
+
+    return $article;
+  }
+
+
+
+  registerLoadMoreButton();
+})();
+
+
+/*
 (function() {
   'use strict';
 
@@ -86,3 +186,5 @@
 
   registerLoadMoreButton();
 })();
+
+*/
